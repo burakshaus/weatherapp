d2d5f3a3feba014d4914f27c0300edf75c411397
Ahmet Burak Can <118999270+burakshaus@users.noreply.github.com>
2025-03-01 22:13:03 +0300

    Initial Commit

---
+++ weatherapp/weatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappApp.swift
+//  weatherapp
+//
+//  Created by Burak  on 1.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct weatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ weatherappTests/weatherappTests.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappTests.swift
+//  weatherappTests
+//
+//  Created by Burak  on 1.03.2025.
+//
+
+import Testing
+@testable import weatherapp
+
+struct weatherappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
